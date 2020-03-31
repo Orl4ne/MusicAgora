3abeb55c4df328c2d699a1e476193ce86cc99d1a
Orlane Vermo <orlane_v@hotmail.com>
2020-03-31 16:03:35 +0200

    Adding Users Service

---
+++ Applicatopn Layer/Users/Users.BLL.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Users.BLL.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Applicatopn Layer/Users/Users.BLL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Users.BLL
+{
+    public class Class1
+    {
+    }
+}
