8f5a71020b1bbb2a0717cc322a921454c3b1f00c
Orlane Vermo <orlane_v@hotmail.com>
2020-04-07 14:18:19 +0200

    Moving identity ApplicationDbContext to DAL

--- Applicatopn Layer/Library/Library.DAL/ApplicationDbContext.cs
+++ Applicatopn Layer/Library/Library.DAL/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace MusicAgora.WebUx.MVC.Data
+namespace Library.DAL
 {
     public class ApplicationDbContext : IdentityDbContext
     {
