294d60153db677ea95b88c40880c93430ff6bd88
Orlane Vermo <orlane_v@hotmail.com>
2020-04-07 14:42:42 +0200

    changing context sql to sqlite and adding nuggets EF Core sqlite

--- Applicatopn Layer/Library/Library.DAL/ApplicationDbContext.cs
+++ Applicatopn Layer/Library/Library.DAL/ApplicationDbContext.cs
@@ -8,9 +8,32 @@ namespace Library.DAL
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public ApplicationDbContext()
+        {
+        }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder is null)
+                throw new ArgumentNullException(nameof(optionsBuilder));
+
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlite(@"Data Source=musicagora.db;");
+                optionsBuilder.EnableSensitiveDataLogging();
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            if (modelBuilder is null)
+                throw new ArgumentNullException(nameof(modelBuilder));
+        }
+        //entities
+        //public DbSet<Student> Students { get; set; }
+        //public DbSet<Course> Courses { get; set; }
     }
 }
