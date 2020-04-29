d76bda030bbf01f26ad32328846b8217d4fbafd9
Orlane Vermo <orlane_v@hotmail.com>
2020-04-29 13:17:23 +0200

    UseCases

---
+++ Applicatopn Layer/Library/Library.BLL/Services/ChiefUC/Chief - GetAllSheets.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.BLL.Services.Chief
+{
+    public partial class Chief
+    {
+    }
+}
---
+++ Applicatopn Layer/Library/Library.BLL/Services/LibrarianUC/Librarian - AddSheetPartToSheet.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.BLL.Services.Librarian
+{
+    public partial class Librarian
+    {
+    }
+}
