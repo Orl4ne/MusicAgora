4680e8f764df8f6e26c62b26ba99089b0f911c16
Orlane Vermo <orlane_v@hotmail.com>
2020-05-16 18:16:36 +0200

    Sheet - UpdateTests

--- Applicatopn Layer/Library/Library.DAL.Tests/RepositoriesTests/SheetRepositoryTests/Sheet - UpdateSheetTests.cs
+++ Applicatopn Layer/Library/Library.DAL.Tests/RepositoriesTests/SheetRepositoryTests/Sheet - UpdateSheetTests.cs
@@ -2,8 +2,10 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MusicAgora.Common.Library.Interfaces.IRepositories;
+using MusicAgora.Common.Library.TransferObjects;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Text;
 
@@ -21,23 +23,29 @@ namespace Library.DAL.Tests.RepositoriesTests.SheetRepositoryTests
                  .Options;
             using var context = new LibraryContext(options);
             ISheetRepository sheetRepository = new SheetRepository(context);
+            ICategoryRepository categoryRepository = new CategoryRepository(context);
 
-            //var category = new CategoryTO { Name = "Musiq de flims" };
-            //var category2 = new CategoryTO { Name = "Musique Classique" };
-            //var category3 = new CategoryTO { Name = "Musique Contemporaine" };
-            //var AddedCategory = categoryRepository.Add(category);
-            //var AddedCategory2 = categoryRepository.Add(category2);
-            //var AddedCategory3 = categoryRepository.Add(category3);
-            //context.SaveChanges();
+            //Act
+            var category = new CategoryTO { Name = "Musique de films" };
+            var addedCategory = categoryRepository.Add(category);
+            var category2 = new CategoryTO { Name = "Musique de classique" };
+            var addedCategory2 = categoryRepository.Add(category2);
+            context.SaveChanges();
 
-            ////Act
-            //AddedCategory.Name = "Musique de films";
-            //var test = categoryRepository.Update(AddedCategory);
-            //context.SaveChanges();
+            var sheet = new SheetTO { Name = "BestOf", Arranger = "Jean-Luc", Category = addedCategory, Composer = "Morricone", IsCurrent = false, IsGarde = false, IsIndependance = true };
+            var sheet2 = new SheetTO { Name = "Young Amadeus", Arranger = "Jan de Haan", Category = addedCategory2, Composer = "Mozart", IsCurrent = true, IsGarde = false, IsIndependance = true };
+            var addedSheet = sheetRepository.Add(sheet);
+            var addedSheet2 = sheetRepository.Add(sheet2);
+            context.SaveChanges();
 
-            ////Assert
-            //Assert.AreEqual(3, categoryRepository.GetAll().Count());
-            //Assert.AreEqual("Musique de films", test.Name);
+            //Act
+            addedSheet.Name = "Old Mozart";
+            var test = sheetRepository.Update(addedSheet);
+            context.SaveChanges();
+
+            //Assert
+            Assert.AreEqual(2, sheetRepository.GetAll().Count());
+            Assert.AreEqual("Old Mozart", test.Name);
         }
         [TestMethod]
         public void UpdateSheet_ProvidingNonExistingSheet_ThrowException()
