d4dc382284b9dcf0d0106898b2d930aa46a3f57d
Orlane Vermo <orlane_v@hotmail.com>
2020-06-08 16:23:28 +0200

    Adding UnitOfWork Interfaces

---
+++ Common/MusicAgora.Common/AccessHelpers/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MusicAgora.Common.AccessHelpers
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        int SaveChanges();
+    }
+}
---
+++ Common/MusicAgora.Common/Library/Interfaces/ILibraryUnitOfWOrk.cs
@@ -0,0 +1,16 @@
+﻿using MusicAgora.Common.AccessHelpers;
+using MusicAgora.Common.Library.Interfaces.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MusicAgora.Common.Library.Interfaces
+{
+    public interface ILibraryUnitOfWOrk : IUnitOfWork
+    {
+        ICategoryRepository CategoryRepository { get; }
+        IInstrumentRepository InstrumentRepository { get; }
+        ISheetRepository SheetRepository { get; }
+        ISheetPartRepository SheetPartRepository { get; }
+    }
+}
