b9de4f83e7462cd111b7042873d6adca245debe5
Orlane Vermo <orlane_v@hotmail.com>
2020-07-27 14:34:54 +0200

    putting injections in controller

--- Presentation Layer/MusicAgora.WebUx.MVC/Controllers/LibraryController.cs
+++ Presentation Layer/MusicAgora.WebUx.MVC/Controllers/LibraryController.cs
@@ -2,13 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Identity.DAL;
+using Library.BLL.UseCases;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MusicAgora.Common.Library.Interfaces;
+using MusicAgora.Common.Library.Interfaces.UseCases;
 
 namespace MusicAgora.WebUx.MVC.Controllers
 {
     public class LibraryController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ILibraryUnitOfWork _libraryUnitOfWork;
+        private readonly IChiefUC _chiefUC;
+        private readonly ILibrarianUC _librarianUC;
+        private readonly IMusicianUC _musicianUC;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+
+        public LibraryController(ILogger<HomeController> logger, ILibraryUnitOfWork unitOfWork, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, IChiefUC chiefUC, ILibrarianUC librarianUC, IMusicianUC musicianUC)
+        {
+            _logger = logger;
+            _libraryUnitOfWork = unitOfWork;
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _chiefUC = chiefUC;
+            _librarianUC = librarianUC;
+            _musicianUC = musicianUC;
+        }
         // GET: LibraryController
         public ActionResult Index()
         {
