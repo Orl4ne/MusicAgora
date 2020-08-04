7f238cb238382b3341809c78b12d76382a7e843f
Orlane Vermo <orlane_v@hotmail.com>
2020-08-04 12:52:13 +0200

    Modifying HomeController index view with Users and roles

--- Presentation Layer/MusicAgora.WebUx.MVC/Controllers/HomeController.cs
+++ Presentation Layer/MusicAgora.WebUx.MVC/Controllers/HomeController.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Identity.DAL;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MusicAgora.Common.Library.Interfaces;
 using MusicAgora.WebUx.MVC.Models;
 
 namespace MusicAgora.WebUx.MVC.Controllers
@@ -12,15 +15,36 @@ namespace MusicAgora.WebUx.MVC.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ILibraryUnitOfWork _libraryUnitOfWork;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly RoleManager<AccessRight> _roleManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,
+                            ILibraryUnitOfWork unitOfWork,
+                            UserManager<ApplicationUser> userManager,
+                            //IdentityRole identityRole,
+                            RoleManager<AccessRight> roleManager,
+                            SignInManager<ApplicationUser> signInManager)
         {
             _logger = logger;
+            _libraryUnitOfWork = unitOfWork;
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var isSignedIn = _signInManager.IsSignedIn(User);
+            ApplicationUser user;
+            if (isSignedIn)
+            {
+                user = _userManager.GetUserAsync(User).Result;
+                return View(user);
+            }
+            user = null;
+            return View(user);
         }
 
         public IActionResult Privacy()
